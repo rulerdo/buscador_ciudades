32655fc852439fe0dc699b6d78cff0ae7f08085d
Raul <rgomezbe@cisco.com>
2021-10-12 16:33:14 -0500

    Agrego Nota de el if que se me olvido

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # Repositorio en GitHub: https://github.com/rulerdo/buscador_ciudades_dev
 # Clonar repo: git clone git@github.com:rulerdo/buscador_ciudades_dev.git
-# Instalar modulo externo tabulate: pip3 install tabulate
 # Datos descargados del sitio: https://worldpopulationreview.com/world-cities
+# Repositorio script terminado: https://github.com/rulerdo/buscador_ciudades
 
 import csv, json, sys
 from tabulate import tabulate
-
 
 def buscar_ciudades(pais,archivo):
 
@@ -14,76 +13,81 @@ def buscar_ciudades(pais,archivo):
 
     try:
 
-        if '.csv' in archivo:
+        if 'csv' in archivo:
 
             with open(archivo,'r') as c:
-                lector_csv = csv.reader(c, delimiter=',')
+
+                lector_csv = csv.reader(c,delimiter=',')
                 ciudades.append(next(lector_csv))
                 for linea in lector_csv:
                     if pais in linea[2]:
                         ciudades.append(linea)
 
-        elif '.json' in archivo:
+
+        elif 'json' in archivo:
 
             with open(archivo,'r') as j:
+
                 lector_json = json.load(j)
                 ciudades.append(list(lector_json[0].keys()))
-                for dict_pais in lector_json:
-                    if pais in dict_pais["Country"]:
-                        ciudades.append(list(dict_pais.values()))
+                for dict_ciudad in lector_json:
+                    if pais in dict_ciudad['Country']:
+                        ciudades.append(list(dict_ciudad.values()))
 
         else:
             raise IndexError
 
-    except (FileNotFoundError,IndexError):
+    except (IndexError,FileNotFoundError):
 
-        print(f'Error: Archivo {archivo} no encontrado o formato no soportado!')
+        print(f'Archivo {archivo} no encontrado o en formato no soportado')
         sys.exit(0)
 
     return ciudades
 
 
 def imprimir_busqueda(ciudades):
-    
+
+    ciudades[0][0] = 'Rank'
+
     total_ciudades = len(ciudades) - 1
-    print(f'Ciudades encontradas:', total_ciudades)
+    print('Ciudades encontradas:',total_ciudades)
 
     if total_ciudades > 0:
-        ciudades[0][0] = 'Rank'
-        print(tabulate(ciudades,headers="firstrow",tablefmt="grid"))
-        print('\n')
+        print(tabulate(ciudades,headers='firstrow',tablefmt='grid'))
 
     return total_ciudades
 
 
-def guardar_busqueda(ciudades,pais):
+def guardar_busqueda(pais,ciudades):
 
-    formato = input('Escribe csv o json para guardar la salida en ese formato o cualquier otro valor para salir sin guardar: ')
-
-    archivo_ciudades = pais + '.' + formato
-    respuesta = f'Archivo guardado como {archivo_ciudades}'
+    formato = input('Escribe csv o json para guardar en ese formato, o cualquier otra cosa para salir: ')
+    archivo_pais = pais + '.' + formato
+    respuesta = f'Archivo guardado como: {archivo_pais}'
 
     if formato == 'csv':
 
-        with open(archivo_ciudades,'w') as f:
-            escritor_csv = csv.writer(f, delimiter=',')
+        with open(archivo_pais,'w') as f:
+
+            escritor_csv = csv.writer(f,delimiter=',')
             for ciudad in ciudades:
                 escritor_csv.writerow(ciudad)
-            
+
+
     elif formato == 'json':
 
         lista_ciudades = list()
         llaves = ciudades.pop(0)
+
         for valores in ciudades:
-            diccionario_ciudad = dict(zip(llaves,valores))
-            lista_ciudades.append(diccionario_ciudad)
+            dict_ciudad = dict(zip(llaves,valores))
+            lista_ciudades.append(dict_ciudad)
 
-        with open(archivo_ciudades,'w') as f:
-            json.dump(lista_ciudades, f, indent=4)
-                            
+        with open(archivo_pais,'w') as f:
+
+            json.dump(lista_ciudades,f,indent=4)
+
     else:
-
-        respuesta = 'Archivo NO guardado'
+        respuesta = 'Archivo NO guardado... adios!'
 
     print(respuesta)
 
@@ -92,10 +96,17 @@ if __name__ == '__main__':
 
     archivo = input('Archivo origen: ')
     pais = input('Pais a buscar: ')
-
     ciudades = buscar_ciudades(pais,archivo)
     total_ciudades = imprimir_busqueda(ciudades)
 
-    if total_ciudades > 0:
-        
-        guardar_busqueda(ciudades,pais)
+    '''
+    RAYOS! Olvide evaluar si total_ciudades era mayor a 0
+    antes de ejecutar la funcion de guardar
+
+    Por favor consideren este update menor en sus codigos
+    Aca la version ya corregida!
+
+    Gracias!
+    '''
+    if total_ciudades > 0: # Esta linea es la que me falto!!!
+        guardar_busqueda(pais,ciudades)
